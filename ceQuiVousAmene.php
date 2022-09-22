95987a408b41e1d151d924df026c1810b67c253b
Zahra Chawki <z.chawki@interface3.be>
2022-09-22 15:44:08 +0200

    version zahra

---
+++ ceQuiVousAmene.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <img src="./assets/img/ciel officiel.png" id="fond_ecran">
+    <title>Now,No Stress</title>
+</head>
+<body>
+   
+    <div class="container">
+        
+
+        <div class="header">
+            <img src="./assets/img/logo_NoStress.png" id="logo">
+        </div>
+            <div>
+                <h2>Qu'est ce qui vous amène sur Now No Stress?</h2>
+            </div>
+             
+       
+       
+        
+        <div class="containt">
+            
+            <div class="bouttons">
+                <div>
+                    <button class="reponses">Mon sommeil</button>
+                </div>
+                
+                <div>
+                    <button class="reponses">Mon anxiété</button>
+                </div>
+                
+                <div>
+                    <button class="reponses">Des tensions</button>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div>
+                <a><img src="./assets/img/retour.png" id="retour"></a>
+            </div>
+            <div class="pasDeCompte">
+                <p>Pas de compte?</p>
+                <p><a>Créer un compte ici</a></p>
+            </div>
+        </div>
+    
+    </div>
