e927c1104e054dc70f32e7ae1968798b3e79c0cd
Zahra Chawki <z.chawki@interface3.be>
2022-09-26 12:38:22 +0200

    finalisation pages Apropos 1et2 et ce quiVousAmène

---
+++ NoStress_zahra/views/aPropos2.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <img src="../assets/img/ciel officiel.png" id="fond_ecran">
+    <title>Now,No Stress</title>
+</head>
+<body>
+   
+    <div class="container">
+        
+        <header>
+            <img src="../assets/img/logo_NoStress.png" id="logo">
+        </header>
+        <div>
+            <h2>A propos de Now No Stress:</h2>
+        </div>
+        
+        <div class="containt">
+            <p>
+                Nous vous offrons l’accès à  une variété de ressources afin de mieux gérer votre stress au              quotidien. De la méditation et la respiration rassemblées au sein d’un espace apaisant et doux.
+                Nous vous souhaitons un doux moment…
+            </p>
+            
+            <button class="btn-questionnaire">Commencer</button>
+        </div>
+        
+        <div>
+                <a href="#"><img src="../assets/img/retour.png" id="retour"></a>
+        </div>
+
+        <div class="pasDeCompte">
+            <br><br><br><p>Pas de compte?<br>
+            <a href="#">Créer un compte ici</a></p>
+        </div>
+    
+   
+
+    </div>
+    
+    
+    
+</body>
+</html>
--- NoStress_zahra/views/ceQuiVousAmene.php
+++ NoStress_zahra/views/ceQuiVousAmene.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/css/style.css">
-    <img src="./assets/img/ciel officiel.png" id="fond_ecran">
+    <link rel="stylesheet" href="../assets/css/ceQuiVousAmene.css">
+    <img src="../assets/img/ciel officiel.png" id="fond_ecran">
     <title>Now,No Stress</title>
 </head>
 <body>
@@ -13,19 +13,16 @@
     <div class="container">
         
 
-        <div class="header">
-            <img src="./assets/img/logo_NoStress.png" id="logo">
-        </div>
+        <header>
+            <img src="../assets/img/logo_NoStress.png" id="logo">
+        </header>
             <div>
                 <h2>Qu'est ce qui vous amène sur Now No Stress?</h2>
             </div>
              
-       
-       
         
         <div class="containt">
             
-            <div class="bouttons">
                 <div>
                     <button class="reponses">Mon sommeil</button>
                 </div>
@@ -37,16 +34,16 @@
                 <div>
                     <button class="reponses">Des tensions</button>
                 </div>
-            </div>
         </div>
-        <div>
+        
             <div>
-                <a><img src="./assets/img/retour.png" id="retour"></a>
+                <a href="#"><img src="../assets/img/retour.png" id="retour"></a>
             </div>
+
             <div class="pasDeCompte">
-                <p>Pas de compte?</p>
-                <p><a>Créer un compte ici</a></p>
-            </div>
+                <p>Pas de compte?<br>
+                <a href="#">Créer un compte ici</a></p>
         </div>
+        
     
     </div>
