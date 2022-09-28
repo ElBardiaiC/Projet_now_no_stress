1a9b019d7aeb34dac4a62e5b4742164c70690a86
Zahra Chawki <z.chawki@interface3.be>
2022-09-28 10:01:37 +0200

    commit

---
+++ NoStress_zahra/assets/scripts/script.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const btnSommeil=document.getElementById('rep1');
+
+btnSommeil.addEventListener('click', () =>{
+  location.href='http://localhost/NOW_NO_S/Projet_now_no_stress/NOWS%20%20HTML%20CSS%20-%20Copy/ProblemeSommeil.html';
+})
+
+// const btnCommencer=document.getElementById('btn-commencer');
+// console.log(btnCommencer);
+
+// btnCommencer.addEventListener('click', () =>{
+//     window.location.href= 'aPropos1.html' ;
+//  })
+
+ const btnQuestionnaire=document.getElementById('questionnaire');
+ console.log(btnQuestionnaire);
+
+btnQuestionnaire.addEventListener('click', () => {
+  window.location.href= 'ceQuiVousAmene.html' ;
+ })
