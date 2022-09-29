8cfaef5ae841aa9826b91aebc8b67fcea83bb717
Zahra Chawki <z.chawki@interface3.be>
2022-09-29 16:34:43 +0200

    ajout de pages solutions-anxiete +reglage du logo

--- NoStress_zahra/assets/scripts/ceQuiVsAmene.js
+++ NoStress_zahra/assets/scripts/ceQuiVsAmene.js
@@ -3,5 +3,11 @@
 const btnSommeil=document.getElementById('rep1');
 
 btnSommeil.addEventListener('click', () =>{
-  location.href='http://127.0.0.1:5500/NOWS_Chaimae/ProblemeSommeil.html';
+  location.href='http://127.0.0.1:5500/NOWS_Chaimae/views/C_ProblemeSommeil.html';
+})
+
+const btnAnxiete=document.getElementById('rep2');
+
+btnAnxiete.addEventListener('click', () =>{
+  location.href='solutionAnxiete.html';
 })
