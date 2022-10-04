9727b496b39853439d85f16971a803a479ae41e3
thais <t.sablon@interface3.be>
2022-10-04 10:06:39 +0200

    T_reparation lien vers Bienvenue

--- NNS_Thais/asset/script/T_versBienvenue.js
+++ NNS_Thais/asset/script/T_versBienvenue.js
@@ -2,5 +2,17 @@ const versBienvenue = document.getElementById('log-in');
 
 versBienvenue.addEventListener('click' , () =>{
 
-    window.location.href='views/T_welcome.html';
+    window.location.href='./views/T_welcome.html';
+})
+
+const essai = document.getElementById('essai');
+
+
+essai.addEventListener('click' , () => {
+
+
+
+window.location.href='../NoStress_zahra/views/aPropos2.html';
+
+
 })
