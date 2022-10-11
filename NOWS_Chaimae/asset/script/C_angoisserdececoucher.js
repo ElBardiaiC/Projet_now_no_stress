fde0126d1a2c408442aa43cb90a5d4eeca037642
Chaimae.ELB <elbardiaichaimae@gmail.com>
2022-10-11 08:02:13 +0200

    essaie menu burger

--- NOWS_Chaimae/asset/script/C_angoisserdececoucher.js
+++ NOWS_Chaimae/asset/script/C_angoisserdececoucher.js
@@ -19,3 +19,35 @@ btnNon.addEventListener('click' , () => {
     
     
     })
+
+    const sidenav = document.getElementById("mySidenav");
+    const openBtn = document.getElementById("openBtn");
+    const closeBtn = document.getElementById("closeBtn");
+    
+   
+    
+    openBtn.addEventListener('click' , (event) => {
+
+ openBtn.onclick = openNav;
+    closeBtn.onclick = closeNav;
+      
+    /* Set the width of the side navigation to 250px */
+    function openNav(event) {
+      
+      if(event){
+        sidenav.classList.add("active");
+      }
+  
+    
+      
+
+      
+    }
+
+    
+         function closeNav() {
+      sidenav.classList.remove("active");}
+    /* Set the width of the side navigation to 0 */
+   
+    }
+)
