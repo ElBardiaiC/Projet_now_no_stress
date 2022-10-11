6cfbc6138082bd7a39eaa954084d970e4d5de11c
Chaimae.ELB <elbardiaichaimae@gmail.com>
2022-10-11 09:24:14 +0200

    C_change

--- NOWS_Chaimae/asset/script/C_angoisserdececoucher.js
+++ NOWS_Chaimae/asset/script/C_angoisserdececoucher.js
@@ -6,7 +6,7 @@ btnOui.addEventListener('click' , () => {
 
 
 
-window.location.href='C_CombiendeTemps.html';
+window.location.href='C_CombiendeTemps.html'
 
 
 })
@@ -20,37 +20,3 @@ btnNon.addEventListener('click' , () => {
     
     })
 
-<<<<<<< HEAD
-=======
-    const sidenav = document.getElementById("mySidenav");
-    const openBtn = document.getElementById("openBtn");
-    const closeBtn = document.getElementById("closeBtn");
-    
-   
-    
-    openBtn.addEventListener('click' , (event) => {
-
- openBtn.onclick = openNav;
-    closeBtn.onclick = closeNav;
-      
-    /* Set the width of the side navigation to 250px */
-    function openNav(event) {
-      
-      if(event){
-        sidenav.classList.add("active");
-      }
-  
-    
-      
-
-      
-    }
-
-    
-         function closeNav() {
-      sidenav.classList.remove("active");}
-    /* Set the width of the side navigation to 0 */
-   
-    }
-)
->>>>>>> ed280e89dcfabc3637e93b9e9fa82491894473d4
