9b8e08262203ac130aebb4927b0386e10a29614b
thais <t.sablon@interface3.be>
2022-10-12 13:01:33 +0200

    T_correction-chemins

---
+++ NNS_Thais/asset/script/menuBurger.js
@@ -0,0 +1,16 @@
+function show() {
+  
+    document.querySelector('.navBar').classList.toggle('active');
+  }
+  
+    let iconMenu = document.getElementById('menuBurger');
+    let toggle=true;
+  
+    iconMenu.addEventListener('click',function(){
+      toggle = !toggle;
+      if(toggle){
+        iconMenu.src='../asset/img/menu-burger.png';
+      }else{
+        iconMenu.src='../asset/img/close-icon.png';
+      }
+    })
