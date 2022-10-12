3cb583e89686e0c1d6c674da35ffa470093be893
Zahra Chawki <z.chawki@interface3.be>
2022-10-12 11:01:32 +0200

    integration menu burger dans les pages de Chaïmae

--- NOWS_Chaimae/asset/script/C_combiendetemps.js
+++ NOWS_Chaimae/asset/script/C_combiendetemps.js
@@ -19,3 +19,22 @@ btnPlus.addEventListener('click' , () => {
     
     
     })
+
+    function show() {
+  
+        document.querySelector('.navBar').classList.toggle('active');
+      }
+      
+        let iconMenu = document.getElementById('menuBurger');
+        let toggle=true;
+      
+        iconMenu.addEventListener('click',function(){
+
+          toggle = !toggle;
+
+          if(toggle){
+                iconMenu.src='../asset/img/Menu-burger new.png';
+          }else{
+                iconMenu.src='../asset/img/Close-icone.png';
+          }
+        })
