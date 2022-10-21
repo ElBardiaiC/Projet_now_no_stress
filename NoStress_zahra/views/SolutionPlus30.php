6ff44cf431b6efcb0c185b96347d22611d09c2ab
Chaimae.ELB <elbardiaichaimae@gmail.com>
2022-10-21 11:26:54 +0200

    C_Rajout 2 page solution

---
+++ NoStress_zahra/views/SolutionPlus30.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/reset.css">
+    <link rel="stylesheet" href="../assets/css/container.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <title>Now,No Stress</title>
+</head>
+<body class="solutions">
+
+    <header>
+            <section>
+                
+                <a href="../../NNS_Thais/T_index.php "><img src="../assets/img/logo_NoStress.png" id="logo"></a>
+                <button class="burger" onclick="show()"><img src="../assets/img/menu-burger.png" alt="" id="menuBurger"></button>
+            
+            </section>
+
+            <div class="navBar-solutions">
+                <h3>Menu</h3>
+                
+                    <ul>
+                        <li><a href="aPropos1.php">A propos de Now no Stress</a></li>
+                        <li><a href="aPropos2.php">Découvrir (Essai rapide)</a></li>
+                        <li><a href="#">Contactez-nous</a></li>
+                        <li><a href="../../NNS_Thais/views/T_createaccount.php">Créez un compte</a></li>
+                    </ul>
+               
+            </div>
+    </header>
+
+    <div class="container">
+        
+
+    
+        <div class="containt-solutions">
+            
+            <div>
+                <h2>Veuillez choisir une solution</h2>
+            </div>
+           
+          
+                <div>
+                    <button class="btn-reponses" id="marche">Séance de yoga de 30 min</button>
+                </div>
+
+                <div>
+                    <button class="btn-reponses" id="huile">Histoire du soir</button>
+                </div>
+        
+                
+        </div>
+        
+    
+    </div>
+        <div id="basPage">
+            
+            <div class="retour">
+                <a href="ceQuiVousAmene.php"><img src="../assets/img/fleche-retour.png"  id="retourFleche"></a>
+            </div>
+            <div class="pasDeCompte">
+                <p>Pas de compte?<br>
+                <a href="../../NNS_Thais/views/T_createaccount.php">Créer un compte ici</a></p>
+            </div>
+        </div>
+    <script src ="../assets/scripts/solutionPlus30.js"></script>
+</body>
+</html>
