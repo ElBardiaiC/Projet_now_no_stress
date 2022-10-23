c737faa1f24158bcb8a608405b5115e379062ca7
Zahra Chawki <z.chawki@interface3.be>
2022-10-23 21:50:30 +0200

    reglage menu burger yoga

--- NoStress_zahra/assets/scripts/yoga_video.js
+++ NoStress_zahra/assets/scripts/yoga_video.js
@@ -2,7 +2,7 @@
  
 function show() {
   
-  document.querySelector('.navBar-solutions').classList.toggle('active');
+  document.querySelector('.navBar').classList.toggle('active');
 }
 
   let iconMenu = document.getElementById('menuBurger');
